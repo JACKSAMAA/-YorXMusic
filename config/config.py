92489277e79baad9c342bf31ac9c341cda0b9325
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-07-29 23:41:51 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 # For customized or modified Repository
-UPSTREAM_REPO = "https://github.com/Unknown-San/LoidSan"
+UPSTREAM_REPO = "https://github.com/Unknown-San/LoidSan1"
 UPSTREAM_BRANCH = "main"
 
 # GIT TOKEN ( if your edited repo is private)
